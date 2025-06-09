99934db07286532b91d81211bf5d80535d549550
Live session user <nehemiahabelcjohan@gmail.com>
2025-06-09 16:52:18 +0100

    End of Monday

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,20 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
+filename="processed_ping_log2.txt"
+label='Ping Task 5, 0.0001s interval'
+xlabel = 'RTT (ms)'
+ylabel = 'Cumulative Probability'
+title='Cumulative Distribution Function'
+fig_name='Ping Task 5, 0.0001s interval.png'
 bins=100 #adjust the number of bins to your plot
 
-
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
+#index_array = [i + 1 for i in range(len(t))]
 
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+#plt.plot(index_array, t, label=label)  # Plot some data on the (implicit) axes.
 #Comment the line above and uncomment the line below to plot a CDF
-#plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
+plt.hist(t, bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
